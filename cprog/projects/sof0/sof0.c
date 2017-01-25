fcb85844f4b21afee7ddd89d3a8f6089ac93cef4
ccattell <ccattell@corning-cc.edu>
2017-01-25 16:48:59 -0500

    worked on sof0 project

---
+++ cprog/projects/sof0/sof0.c
@@ -0,0 +1,33 @@
+/* sof0 Project for C/C++ class */
+/* Christian Cattell | ccattell | ccattell@corning-cc.edu */
+
+#include <stdio.h> /* imports stdio.h library */
+
+
+
+
+
+int main()
+{
+	unsigned short userInput;
+	unsigned short onesPlace;
+	unsigned short increment;
+
+	printf ("Enter a number ending in 5 (MAX 995)\n"); /* Prompt user for input */
+	scanf ("%d", &userInput); /* scans stdin for an integer, then stores the value at variable userInput */
+	if(userInput > 995 && userInput
+
+
+
+
+
+
+
+
+
+
+	return 0; 		
+
+
+}
+
