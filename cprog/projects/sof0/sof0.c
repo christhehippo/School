7514102cda8fb302b2d08230a931ef97ff1f4bcc
ccattell <ccattell@corning-cc.edu>
2017-01-27 17:02:32 -0500

    final ver sof0.c

--- cprog/projects/sof0/sof0.c
+++ cprog/projects/sof0/sof0.c
@@ -10,39 +10,41 @@
 /*Starts the program. */
 int main() 
 {
-	/* Initializes and creates memory addresses for the variables used in the program. The scope of these variables is everything in intMain. */
+	/* Where the user input will be stored. */
 	unsigned short userInput; 
-	unsigned short inc; 
-	unsigned short productHalf1;
-	unsigned short productHalf2;
 	
 	
- 	/*productHalf1 = userInput - 5;
-	productHalf1 = increment / 10;
-	productHalf2 = userInput - increment; */
-
-
-
-	/* "LOOP:" is where the program is sent if an unsupported value is entered. Prompts user to enter their value with instructions. */
-	LOOP: printf ("This program calculates the square root of a number ending in 5, up to 955. Enter a a number ending in 5:\n"); 
+ 	/*  "LOOP:" is where the program is sent if an unsupported value is entered. Prompts user to enter their value with instructions. */
+	LOOP:fprintf(stderr, "Enter value: "); 
 	
 	/* scanf takes the user entered value and sets the value of the short int userInput to that collected value. */
-	scanf ("%hu", &userInput); /*how to insert multiple scanf functions to define many variables?????????????????????????///// */
-	/* 7/2=3, 7%2=1
+	fscanf(stdin, "%hu", &userInput); 
 	/* If the value of userInput is above 995 OR below 5, then the body of this loop will be executed. The body prints the error message, then has the program go back to the above user prompt. */
 	if (userInput > 995 | userInput < 5 | userInput % 5 > 0)
 	{
-		/* Tells the system to print the following string, given that one of the conditions in the if statement is true. */
-		printf ("Invalid response, try again.(MIN:5 MAX:955, NO DECIMALS)\n");
+		
+		fprintf (stdout, "Invalid response, try again.(MIN:5 MAX:955, NO DECIMALS)\n");
 
-		/* Tells the program to go back to the step labeled "LOOP" and continue on from there. */
+		
 		goto LOOP; 
 
 		
 	}
+	
+	/* Define variables used in calculating and displying expected output. */	
+	unsigned short n = userInput - 5;
+	unsigned short inc =  n / 10;
+	unsigned short half1 =  inc * (inc + 1); 
+	unsigned short x = userInput - n;
+	unsigned short half2 = x * x;
 
-	printf ("%d, %d", userInput, inc);
 
+
+	/*I used the below code for debug*/
+	/* printf ("%hu, %hu, %hu, %hu\n", userInput, inc, half1, half2);*/
+	fprintf(stderr, "%-3hu x %-3hu = ", userInput, userInput);
+	fprintf(stdout, "%4hu%2hu\n", half1, half2);
+	
 
  
 
