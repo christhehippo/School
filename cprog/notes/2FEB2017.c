aff73a022af4374aa5debbe0c92220e2d49e97e7
ccattell <ccattell@corning-cc.edu>
2017-02-02 14:48:12 -0500

    yea

---
+++ cprog/notes/2FEB2017.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
+
+
+int main (int argc, char **argv)
+{
+	
+
+
+
+	return (0);
+}
---
+++ cprog/projects/dow0/dow0.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+
+
+unsigned short inputYear;
+
+fprintf(stderr, "Which year: ");
+fscanf(stdin, "%hu", &inputYear);
+break;
+
+unsigned short nYear = (inputYear % 100);
+
+
+
+
+
+
+/*fprintf(stdout, "%hu\n", inputYear);*/
+/*fprintf(stdout, "%hu\n", nYear);
+return (0);
+}
