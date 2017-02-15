52a6a8ce597355d8bda606bd483b33724204785e
ccattell <ccattell@corning-cc.edu>
2017-02-14 23:42:27 -0500

    pbx1 shhhhtuff

--- cprog/projects/mbe0/mbe0.c
+++ cprog/projects/mbe0/mbe0.c
@@ -20,10 +20,12 @@ int main ()
 	fprintf(stderr, "Enter value: ");
 	fscanf(stdin, "%u", &userInput);
     
-	//if 2 digit, followed by else 3 digit
+	
 
 	//***NOTE I MAY HAVE SOME USELESS STUFF IN HERE, I CUT AND OPTIMIZED SO I
 	//MAY HAVE MISSED SOME STUFF BUT HEY IT WORKS***
+	
+	//if input is 2 digits, else assume input is 3 digits
 	if (userInput < 100)
 	{
 	
@@ -84,24 +86,59 @@ int main ()
 		fprintf(stdout, "%6hhu\n", sum1);
 		}
 		//next line
-		fprintf(stderr, "        = %hhu%hhu%hhu\n", sum100, sum10, sum1);		
+		fprintf(stderr, "        =  %hhu%hhu%hhu\n", sum100, sum10, sum1);		
 	}
+	//3 digit userInput 
 	else
 	{
 		unsigned char d1 = (userInput / 100);
 		unsigned char d2 = ((userInput - ((d1 * 100))) / 10);
+		unsigned char d1d2 = (d1 + d2);
 		unsigned char d3 = (userInput - ((d1 * 100) + (d2 * 10)));
-
-
-
+		unsigned char d2d3 = (d2 + d3);
+		//pointless BUT helps me clarify what I am working with while conforming to specs
+		sum1 = d3;
+		carry1 = (d3 / 10);
+		//check for carry	
+		if (d2d3 >= 10)
+		{
+			sum10 = (d2d3 - 10);
+		}
+		else 
+		{
+			sum10 = d2d3;
+		}
+		carry10 = (d2d3 / 10);
+		//another check for carry
+		if (d1d2 >= 10)
+		{
+			sum100 = ((d1d2 - 10) + carry10);
+		}
+		else
+		{
+			sum100 = (d1d2 + carry10);
+		}
+		carry100 = (d1d2 / 10);
+		//check 'em
+		if ((d1 >= 9) && (carry100 >= 1)) 
+		{
+			sum1000 = 0;
+			carry1000 = 1;
+		}
+		else
+		{
+			sum1000 = (d1 + carry100);
+			carry1000 = 0;
+		}
+		sum10000 = carry1000;
+		//math done, ready for output
+		fprintf(stderr, "%u x 11 = ", userInput);
+			
 
 
 		fprintf(stderr, "d1: %hhu, d2: %hhu, d3: %hhu\n", d1, d2, d3);
+		fprintf(stderr, "%hhu %hhu %hhu %hhu %hhu", sum10000, sum1000, sum100, sum10, sum1);
 	}
-	
-
-		
-	
 	
 	return (0);
 }
