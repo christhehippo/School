842b576fa270dac355891b93bdce2a02d7c6bbe0
ccattell <ccattell@corning-cc.edu>
2017-08-21 16:00:04 -0400

    hello.c

--- cprog/projects/cci0/hello.c
+++ cprog/projects/cci0/hello.c
@@ -1,2 +1,13 @@
 /*
+* hello.c - the first best C program ever
 *
+* written by: Christian Cattell
+*/
+#include <stdio.h>
+
+int main()								// ever program needs a start, main() is ours
+{
+	fprintf(stdout, "Hello, World!\n");  // display a message to STDOUT
+	return (0);							// return a sccess status to OS
+}
+
