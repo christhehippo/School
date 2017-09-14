f8fcd67ca3b58720cdb6c126a4559f2be3ca56de
ccattell <ccattell@corning-cc.edu>
2017-09-14 18:24:12 -0400

    finished two digit part of mbe0

--- cprog/projects/mbe0/mbe0.c
+++ cprog/projects/mbe0/mbe0.c
@@ -13,17 +13,16 @@ int main ()
 
 	fprintf(stderr, "Enter value: ");
 	fscanf(stdin, "%hu", &input);
-	
-//	fprintf(stderr, "Input: %hu, Input / 100: %hu\n", input, (input / 100));
+
+	//initial if statement to determine if processing a two or three digit number
+	//if = 2 or less digits, else = 3 or more digits
 	if ((input / 100) == 0)
 	{
 		//first piece output
 		fprintf(stderr, "%3hu x 11 = ", input);
 
 		sum1 = (input % ((input / 10) * 10)); 
-		carry1 = 0;
-//		fprintf(stderr, "sum1: %d\n", sum1);
-
+		carry1 = 0; //this has no use, I don't know why it was required to have it
 		sum10 = (input / 10) + sum1;
 		carry10 = 0;
 		sum100 = input / 10;
@@ -34,6 +33,7 @@ int main ()
 		}
 		else
 		{
+			//no carries, then this path is taken
 			fprintf(stderr, "%15d   ", sum100);
 			fprintf(stderr, "(%d+%d)", sum100, sum1);
 			fprintf(stderr, "%4d\n", sum1);
@@ -48,24 +48,46 @@ int main ()
 //		fprintf(stderr, "sum10: %d\n", sum10);
 //		fprintf(stderr, "carry10: %d\n", carry10);		
 		sum100 = (input / 10) + carry10;
-
+		carry100 = sum100 / 10;
 		if (sum100 >= 10)
 		{
-			carry100 = sum100 / 10;
+			//if the hundreds place has a carry this path is taken
 			sum100 = sum100 - 10;
-		}
-		else
-		{
+
 			fprintf(stderr, "%15d   ", (input / 10));
-			fprintf(stderr, "(%d+%d)", sum100, sum1);
+			fprintf(stderr, "(%d+%d)", (input / 10), sum1);
 			fprintf(stderr, "%4d\n", sum1);
 			fprintf(stderr,"         = ");
 			fprintf(stderr,"%15d", (input / 10));
-			fprintf(stderr,"%6d", (sum10 + 10));
+			fprintf(stderr,"%5d%d", carry100, sum10);
 			fprintf(stderr,"%6d\n", sum1);
 			fprintf(stderr,"         = ");
 			fprintf(stderr,"            (%d+%d)", (input / 10), carry10);
 			fprintf(stderr,"%4d", sum10);
+			fprintf(stderr,"%6d\n", sum1);
+			fprintf(stderr,"         = ");
+			fprintf(stderr,"%14d%d", carry100 ,sum100);
+			fprintf(stderr,"%6d", sum10);
+			fprintf(stderr,"%6d\n", sum1);
+			fprintf(stderr,"         = ");
+			fprintf(stdout,"%d%d%d%d\n", carry100,sum100, sum10, sum1);
+			return (0);
+
+		}
+		
+		else
+		{
+			//this path is for if there is a carry from the tens place calculation 
+			fprintf(stderr, "%15d   ", (input / 10));
+			fprintf(stderr, "(%d+%d)", (input / 10), sum1);
+			fprintf(stderr, "%4d\n", sum1);
+			fprintf(stderr,"         = ");
+			fprintf(stderr,"%15d", (input / 10));
+			fprintf(stderr,"%5d%d", carry10, sum10);
+			fprintf(stderr,"%6d\n", sum1);
+			fprintf(stderr,"         = ");
+			fprintf(stderr,"            (%d+%d)", (input / 10), carry10);
+			fprintf(stderr,"%4d",sum10);
 			fprintf(stderr,"%6d\n", sum1);
 			fprintf(stderr,"         = ");
 			fprintf(stderr,"%15d", sum100);
@@ -76,15 +98,7 @@ int main ()
 			return (0);
 
 		}
-		
-		
-//		fprintf(stderr, "sum100: %d\n", sum100);
-//		fprintf(stderr, "carry100: %d\n", carry100);
-		
-//		sum1000 = carry100;
-//		fprintf(stderr, "sum1000: %d\n", sum1000);
-
-	}
+	}		
 	else
 	{
 
