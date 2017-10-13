a9630d52fc047b977d3dfc168cdde4f4f7949a93
ccattell <ccattell@corning-cc.edu>
2017-10-13 17:32:18 -0400

    made isoArray start at 0 instead of 1

--- cprog/projects/mbe1/mbe1.c
+++ cprog/projects/mbe1/mbe1.c
@@ -28,25 +28,29 @@ int main()
 //		fprintf(stdout, "digitLoop: %hhu\n", digitLoop);
 	}
 	
-//	fprintf(stdout, "digitCount: %u\n", digitCount);
+	fprintf(stdout, "digitCount: %u\n", digitCount);
 
 	unsigned char isoInput[digitCount];	  //Create array with a size of the number of digits of input
 
 	isoFill = digitCount;
-	for (isoFill = digitCount; isoFill > 0; isoFill--)
+
+	do
 	{
+		isoFill--;
 		isoInput[isoFill] = (unsigned char)(input % 10); //Gives values to 1-N in array
-		input = input / 10;;
+		input = input / 10;
 		fprintf(stdout, "isoInput[%hhu] is: %hhu\n", isoFill, isoInput[isoFill]);
 	}
-	
-	initialSpace = 9 - digitCount; 						 //Adds this number of spaces,
-	fprintf(stdout, "%*hhu", initialSpace, isoInput[1]); //changes based on input, supports up to 8.
+	while (isoFill != 0);
+
+
+	initialSpace = 9 - digitCount; 						 	 //Adds this number of spaces,
+	fprintf(stdout, "%*hhu", initialSpace, isoInput[0]);     //changes based on input, supports up to 8.
 	
 ////////////////////////////////////////////////////////////////
 //					Breaks here
 //
-//	for (isoPrint = 2; isoPrint <= digitCount; isoPrint++)
+//	for (isoPrint = 1; isoPrint < 3; isoPrint++)
 //	{
 //		fprintf(stdout, "%hhu", isoInput[isoPrint]);
 //	}
