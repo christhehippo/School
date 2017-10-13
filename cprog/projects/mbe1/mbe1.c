8a0ab326bd6a92f348b1a15bd6a1b9ccf53c11fb
ccattell <ccattell@corning-cc.edu>
2017-10-13 14:38:36 -0400

    mbe1 work nothing really done just a couple loop changes

--- cprog/projects/mbe1/mbe1.c
+++ cprog/projects/mbe1/mbe1.c
@@ -16,37 +16,42 @@ int main()
 {
 	//Initial Variables
 	unsigned int input, digitLoop;
-	unsigned char digitCount, isoFill;
+	unsigned char digitCount, isoFill, initialSpace, isoPrint;
 
 	fprintf(stderr, "Enter value: "); 	  //Obtain user input
 	fscanf(stdin, "%u", &input);
-
 	digitLoop = input;					  //Getting number of input digits
-	while (digitLoop != 0) 				  //This is used for input array size
+	for (digitLoop = input; digitLoop > 0; digitLoop = digitLoop / 10)  //This is used for input array size
 	{
 		digitCount++;
-		digitLoop = digitLoop / 10;
-		fprintf(stdout, "digitLoop: %u\n", digitLoop);
+//		fprintf(stdout, "digitLoop: %hhu\n", digitLoop);
 	}
 	
+//	fprintf(stdout, "digitCount: %u\n", digitCount);
 
-	fprintf(stdout, "digitCount: %u\n", digitCount);
-
-
-	unsigned char isoInput[digitCount];	  //Create array of the input
+	unsigned char isoInput[digitCount];	  //Create array with a size of the number of digits of input
 
 	isoFill = digitCount;
-	while (isoFill > 0)
+	for (isoFill = digitCount; isoFill > 0; isoFill--)
 	{
 		isoInput[isoFill] = (unsigned char)(input % 10); //Gives values to 1-N in array
 		input = input / 10;;
-		fprintf(stdout, "isoInput[%u] is: %u\n", isoFill, isoInput[isoFill]);
-		isoFill--;
+		fprintf(stdout, "isoInput[%hhu] is: %hhu\n", isoFill, isoInput[isoFill]);
 	}
 	
-	unsigned char initialSpace = 9 - digitCount;
-	fprintf(stdout, "%*u\n", initialSpace, isoInput[1]); //Variable spacing for input
-//	fprintf(stdout, "digitCount: %u\n", digitCount);
+	initialSpace = 9 - digitCount; 						 //Adds this number of spaces,
+	fprintf(stdout, "%*hhu", initialSpace, isoInput[1]); //changes based on input, supports up to 8.
+	
+////////////////////////////////////////////////////////////////
+//					Breaks here
+//
+//	for (isoPrint = 2; isoPrint <= digitCount; isoPrint++)
+//	{
+//		fprintf(stdout, "%hhu", isoInput[isoPrint]);
+//	}
+///////////////////////////////////////////////////////////////	
+	fprintf(stdout, "\n");	
+	
 	
 
 	
